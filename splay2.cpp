212573e02c4ad1dca7abe1a0bdcd8153c623220a
Agata <amomot@bio-prodict.nl>
2021-12-30 20:38:44 +0100

    Cenne testy ze szkopułu

--- splay2.cpp
+++ splay2.cpp
@@ -793,6 +793,7 @@ int main(void) {
     int num_commands;
     commands com;
     string dna = read_input(word_length, num_commands, com);
+    cout << dna;
     print_commands(com);
 
     DNAzer result;
