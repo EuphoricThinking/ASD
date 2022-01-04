86e7c5b3b3b91a2bb3d9ace724939c8f7750fc65
Agata <amomot@bio-prodict.nl>
2022-01-04 23:00:49 +0100

    I have to change to vector<int, point>

--- kap.cpp
+++ kap.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
 using std::pair;
 using std::unordered_map;
@@ -14,6 +15,7 @@ using std::cin;
 using std::endl;
 
 using std::make_pair;
+using std::sort;
 
 using point = pair<int, int>;
 using neighbours = vector<point>;
@@ -47,6 +49,27 @@ void print_points(points pp) {
 void print_input(int num_islands, points pp) {
     cout << num_islands << endl;
     print_points(pp);
+}
+
+void insert_into_graph(graph &islands, point current_island, point island_to_insert) {
+    graph::iterator found = islands.find(current_island);
+
+    if (found == islands.end()) {
+        points new_points;
+        new_points.push_back(island_to_insert);
+        islands.insert(make_pair(current_island, new_points));
+    } else {
+        points &old_points = found->second;
+        old_points.push_back(island_to_insert);
+    }
+}
+graph sort_coordinates_and_insert_into_graph(points pp) {
+    sort(pp.begin(), pp.end());
+
+    print_points(pp);
+    graph result;
+
+    result
 }
 
 int main(void) {
