f47be36e22a725da8b764ef51743adc2239b039d
Agata <amomot@bio-prodict.nl>
2022-01-06 19:40:56 +0100

    Before assignment of the indexes in the proper dbf

--- lex.cpp
+++ lex.cpp
@@ -16,6 +16,7 @@ using std::string;
 using std::make_pair;
 using std::floor;
 using std::log;
+using std::pow;
 
 using indexes = pair<int, int>;
 using command = pair<indexes, indexes>;
@@ -25,10 +26,11 @@ using index_in_word = int;
 using sorting_pack = pair<index_in_word, indexes>;
 using sorter = vector<vector<vector<sorting_pack>>>;
 using dbf = vector<vector<int>>;
-using temp_indexes = vector<indexes>;
+using temp_indexes = vector<sorting_pack>;
 
 const int MAX_LENGTH = 300000;
 const int NUM_LETTERS = 26;
+const int A_POSITION = 96;
 
 commands read_input(int &word_length, int &num_commands, string &word) {
     cin >> word_length >> num_commands >> word;
@@ -98,11 +100,42 @@ dbf create_dbf(int word_length, int floor_log_length){
     return result;
 }
 
+int get_alphabet_index(char c) {
+    return int(c) - A_POSITION;
+}
+
+void initialize_dbf(dbf &table, int word_length, string s) {
+    for (int i = 0; i < word_length; i++) {
+        table[i][0] = get_alphabet_index(s[i]);
+    }
+}
+
+void radix_sort(sorter &sorted, temp_indexes temp) {
+    for (sorting_pack sp: temp) {
+        indexes coordinates = sp.second;
+        sorted[coordinates.first][coordinates.second].push_back(sp);
+    }
+}
+
 dbf fill_dbf_table(int word_length, string s) {
     int floor_log_length = (int)floor(log(word_length));
     dbf result = create_dbf(word_length, floor_log_length);
+    sorter sorted = initialize_sorter(word_length);
+    initialize_dbf(result, word_length, s);
     temp_indexes temp_to_sort;
+    indexes indexes_to_sort;
+    sorting_pack index_and_pair_of_indexes;
 
+    for (int j = 1; j < floor_log_length; j++) {
+        for (int i = 0; i < word_length; i++) {
+            indexes_to_sort = make_pair(result[i][j - 1],
+                                        result[i + pow(2, j - 1)][j - 1]);
+            index_and_pair_of_indexes = make_pair(i, indexes_to_sort);
+            temp_to_sort.push_back(index_and_pair_of_indexes);
+        }
+
+        radix_sort(sorted, temp_to_sort);
+    }
 }
 
 int main() {
