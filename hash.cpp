db89bdf6ee0d1da4c0d762202f8fb78e9cf36c47
Agata <amomot@bio-prodict.nl>
2022-01-16 22:00:15 +0100

    Does it really work?

--- hash.cpp
+++ hash.cpp
@@ -128,60 +128,83 @@ int find_longest_common_prefix(const command &com, const vec_int &powers, const 
     //using only indexes, thus we have l = 0;
     int l = 0;
     int r = min(s1_r - s1_l + 1, s2_r - s1_l + 1);
+    cout << "s1_l " << s1_l << " s1_r " << s1_r << " s2_l " << s2_l <<
+        " s2_r " << s2_r << " r " << r << endl;
     int mid;
     int hash_s1;
     int hash_s2;
     while (l < r) {
         mid = (l + r)/2;
+        cout << "l " << l << " | r " << r << " | mid " << mid << endl;
         hash_s1 = calculate_hash_value(s1_l, s1_l + mid - 1, hash_table, powers);
         hash_s2 = calculate_hash_value(s2_l, s2_l + mid - 1, hash_table, powers);
+        cout << "s1_l, s1_l + mid - 1 : " << s1_l << " " << s1_l + mid - 1;
+        cout << "s2_l, s2_l + mid - 1 : " << s2_l << " " << s2_l + mid - 1;
 
         if (hash_s1 == hash_s2) {
+            cout << "UP" << endl;
             l = mid + 1;
         } else {
             r = mid;
         }
     }
 
-    if (l == r) {
+ /*   if (l == r) {
         return -1;
     } else {
         return l;
+    }*/
+    return l;
+}
+
+void print_result(const string &word, int ind1, int ind2, int len1, int len2) {
+    int val1 = value_of_a_letter(word[ind1]);
+    int val2 = value_of_a_letter(word[ind2]);
+    if ( val1 < val2) {
+        cout << SMALLER << "\n";
+    }
+    else if (val1 > val2){
+        cout << GREATER << "\n";
+    } else {
+        if (len1 == len2) cout << EQUAL << "\n";
+        else if (len1 < len2) cout << SMALLER << "\n";
+        else cout << GREATER << "\n";
     }
 }
 
-void print_result(const string &word, int ind1, int ind2) {
-    if (value_of_a_letter(word[ind1]) <
-        value_of_a_letter(word[ind2])) {
-        cout << SMALLER << "\n";
-    }
-    else {
-        cout << GREATER << "\n";
-    }
-}
 void compare_subwords(const string &word, const vec_int &powers, const vec_int &hash_table,
                       const command &com) {
     int index_first_different = find_longest_common_prefix(com, powers, hash_table);
-    if (index_first_different == -1) {
-        cout << EQUAL << "\n";
+    int len1 = com.first.second - com.first.first;
+    int len2 = com.second.second - com.second.first;
+    cout << "\nL: " << index_first_different << "\n\n";
+
+/*    if (index_first_different == -1) {
+        if (len1 == len2) cout << EQUAL << "\n";
+        else if (len1 < len2) cout << SMALLER << endl;
+        else cout << GREATER << endl;
     }
-    else {
-        print_result(word, com.first.first + index_first_different - 1,
-                     com.second.first + index_first_different - 1);
-    }
+    else {*/
+        print_result(word, com.first.first + index_first_different,
+                     com.second.first + index_first_different , len1, len2); //-1 from index
+ //   }
 }
 
 void execute_commands(const string &word, const vec_int &powers, const vec_int &hash_table,
                       const commands &coms) {
     for (command com: coms) {
+        cout << "NEW\n\n" << endl;
         compare_subwords(word, powers, hash_table, com);
+        cout << "\n\n";
     }
 }
 
 int main() {
+    //freopen("lex_test1.in","r",stdin);
     int word_length, num_commands;
     string word;
     commands com = read_input(word_length, num_commands, word);
+    print_input(word_length, num_commands, word, com);
 
     vec_int hash_table = calculate_hash_table(word, word_length);
     vec_int powers = calculate_powers(word_length);
