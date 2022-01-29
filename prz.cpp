d8c63519f1871271a2cb69b6fefc59ea294d5010
Agata <amomot@bio-prodict.nl>
2022-01-29 02:01:55 +0100

    Finisjed add, but noot tested

--- prz.cpp
+++ prz.cpp
@@ -17,12 +17,17 @@ using std::pair;
 
 using std::make_tuple;
 using std::get;
+using std::make_pair;
+using std::min;
 
-using command = tuple<char, int, int>;
-using list_of_commands = vector<command>;
 using ixes = set<int>;
 using interval = pair<int, int>;
 using set_of_intervals = set<interval>;
+using ixes_track = pair<ixes, int>;
+
+using command = pair<char, interval >;
+using list_of_commands = vector<command>;
+
 
 
 list_of_commands read_input(int &U_range, int &num_commmands) {
@@ -35,15 +40,19 @@ list_of_commands read_input(int &U_range, int &num_commmands) {
 
     for (int i = 0; i < num_commmands; i++) {
         cin >> temp_char >> temp_left >> temp_right;
-        result.push_back(make_tuple(temp_char, temp_left, temp_right));
+        result.push_back(make_pair(temp_char, make_pair(temp_left, temp_right)));
     }
 
     return result;
 }
 
+void print_interval(interval inter) {
+    cout << inter.first << " " << inter.second << endl;
+}
 void print_commands(list_of_commands &coms) {
     for (command com: coms) {
-        cout << get<0>(com) << " " << get<1>(com) << " " << get<2>(com) << endl;
+        cout << com.first << " ";
+        print_interval(com.second);
     }
 }
 
@@ -52,7 +61,25 @@ void print_input(int U_range, int num_commands, list_of_commands &coms) {
     print_commands(coms);
 }
 
-void add_interval
+void add_interval(interval inter, set_of_intervals &current, ixes_track &prefix,
+                  ixes_track &suffix, int &num_outside) {
+    if (current.insert(inter).second) {
+        if (inter.first == 1) {
+            if (prefix.second < inter.second) {
+                prefix.second = inter.second;
+                prefix.first.insert(inter.second);
+            }
+        } else {
+            if (suffix.second > inter.first) {
+                suffix.second = inter.first;
+                suffix.first.insert(inter.first);
+            }
+        }
+
+        cout << min(0, suffix.second - prefix.second) << endl;
+    }
+}
+
 int main() {
     int U_range;
     int num_commands;
@@ -60,9 +87,14 @@ int main() {
     print_input(U_range, num_commands, commands);
 
     int num_outside_U;
-    ixes prefixes;
-    ixes suffixes;
+    ixes pref_pre;
+    ixes suff_pre;
     set_of_intervals current_intervals;
+
+    ixes_track prefixes = make_pair(pref_pre, 0);
+    ixes_track suffixes = make_pair(suff_pre, 0);
+
+
 }
 
 
