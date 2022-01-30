b80c51c7b108954693a4bc83699478b44fd0bf0f
Agata <amomot@bio-prodict.nl>
2022-01-30 10:02:14 +0100

    At this moment returns correct results

--- inw2.cpp
+++ inw2.cpp
@@ -27,7 +27,7 @@ vec_int read_input_find_components(int &num_nodes) {
         components.push_back(make_pair(1, 1));
         comp_size++;
     }
-    cout << cur_node << endl;
+//    cout << cur_node << endl;
     int max = cur_node;
     int counter = 1;
     for (int i = 1; i < num_nodes; i++) {
@@ -42,9 +42,9 @@ vec_int read_input_find_components(int &num_nodes) {
                 components.push_back(make_pair(cur_node, counter - 1));
             } else {
                 data &prev = components[comp_size - 1];
-                cout << "count " << counter << endl;
+ //               cout << "count " << counter << endl;
                 prev = make_pair(prev.first, counter - 1);
-                cout << "here " << prev.first << " " << prev.second << endl;
+ //               cout << "here " << prev.first << " " << prev.second << endl;
                 components.push_back(make_pair(cur_node, counter - 1));
             }
 
@@ -78,38 +78,37 @@ void print_content_of_components(int beg, int end) {
     cout << endl;
 }
 
-//void print_result_components(vec_int &components, int num_nodes) {
-//    int comp_lenght = components.size();
-//
-//    cout << "l " << comp_lenght + 1 << endl;
-//
-//    if (!components.empty()) {
-//        int prev = 1;
-//        int beg = 0;
-//        if (components[0] == 1) {
-//            cout << "1 1\n";
-//            beg = 1;
-//        }
-//
-//        for (int i = beg; i < comp_lenght; i++) {
-//            print_content_of_components(prev, components[i]);
-//            prev = components[i];
-//        }
-//
-//        print_content_of_components(prev, num_nodes + 1);
-//    } else {
-//        cout << 1;
-//        for (int i = 1; i <= num_nodes; i++) {
-//            cout << " " << i;
-//        }
-//
-//        cout << endl;
-//    }
-//}
+void print_result_components(vec_int &components, int num_nodes) {
+    int comp_lenght = components.size();
+
+    //cout << "l " << comp_lenght + 1 << endl;
+
+    if (comp_lenght != 0) {
+        if (components[0].first != 1) {
+            cout << comp_lenght + 1 << endl;
+            print_content_of_components(1, components[0].first);
+        } else {
+            cout << comp_lenght << endl;
+        }
+
+        data prev;
+        for (int i = 0; i < comp_lenght; i++) {
+            prev = components[i];
+            print_content_of_components(prev.first - prev.second + 1, prev.first + 1);
+        }
+    } else {
+        cout << 1 << endl;
+        for (int i = 1; i <= num_nodes; i++) {
+            cout << " " << i;
+        }
+
+        cout << endl;
+    }
+}
 
 int main() {
     int num_nodes;
     vec_int components = read_input_find_components(num_nodes);
-    print_components(components);
-    //print_result_components(components, num_nodes);
+    //print_components(components);
+    print_result_components(components, num_nodes);
 }
